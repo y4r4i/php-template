993adf528cdff201ca04fe0f06c5543d7134321f
Y4r4i <110506666+y4r4i@users.noreply.github.com>
2023-02-08 05:55:13 +0900

    add assets

---
+++ assets/.eslintrc.cjs
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard-with-typescript',
+    "prettier"
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json']
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+    "react/jsx-uses-react": "off",
+    "react/react-in-jsx-scope": "off"
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+}
