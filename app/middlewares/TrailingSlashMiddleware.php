b44181b592f9a372e2159757b1a92b53cb12ddf3
Y4r4i <110506666+y4r4i@users.noreply.github.com>
2023-05-22 10:32:28 +0900

    fix trailing slash

--- app/middlewares/TrailingSlashMiddleware.php
+++ app/middlewares/TrailingSlashMiddleware.php
@@ -10,29 +10,37 @@ use Slim\Psr7\Response;
 
 class TrailingSlashMiddleware
 {
-    private string $basePath;
+
+    private BasePath $basePath;
+
 
     public function __construct(BasePath $basePath)
     {
-        $this->basePath = $basePath();
-    }
+        $this->basePath = $basePath;
+
+    }//end __construct()
+
 
     public function __invoke(Request $request, RequestHandler $handler): Response|Message|ResponseInterface
     {
-        $uri = $request->getUri();
-        $path = str_replace($this->basePath, "", $uri->getPath());
-        if ($path != '/' && str_ends_with($path, '/')) {
-            $path = rtrim($path, '/');
-            $uri = $uri->withPath($path);
-            if ($request->getMethod() == 'GET') {
-                $response = new Response();
-                return $response
-                    ->withHeader('Location', (string)$uri)
-                    ->withStatus(301);
-            } else {
-                $request = $request->withUri($uri);
+        $uri  = $request->getUri();
+        $path = $uri->getPath();
+        if ($this->basePath->__invoke().'/' != $path) {
+            if ($path != '/' && str_ends_with($path, '/')) {
+                $path = rtrim($path, '/');
+                $uri  = $uri->withPath($path);
+                if ($request->getMethod() == 'GET') {
+                    $response = new Response();
+                    return $response->withHeader('Location', (string) $uri)->withStatus(301);
+                } else {
+                    $request = $request->withUri($uri);
+                }
             }
         }
+
         return $handler->handle($request);
-    }
-}
+
+    }//end __invoke()
+
+
+}//end class
